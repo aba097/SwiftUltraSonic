aaba15ac962428befff7bc38dbb0947a5797802b
aba <socceraba2701@gmail.com>
2021-10-07 09:41:06 +0900

    ViewController

---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tmp
+//
+//  Created by 相場智也 on 2021/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var ultrasonic = UltraSonic()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+            
+        //再生する
+        ultrasonic.startSound()
+        
+        //再生を停止する
+        //ultrasonic.stopSound()
+    
+    }
+    
+}
+
